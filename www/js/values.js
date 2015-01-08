7b9ae40a40b743cdf3a60987aa5fd8070d663824
Adam Barnhard <admbarn@gmail.com>
2015-01-08 16:09:58 -0600

    updated origin to deployed heroku app

--- www/js/values.js
+++ www/js/values.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular.module('trainer.values', [])
-  //.value('origin', 'https://shrouded-citadel-8762.herokuapp.com');
-  .value('origin', 'http://MPro-2.local:3000');
+  .value('origin', 'https://sleepy-headland-6364.herokuapp.com');
+  //.value('origin', 'http://MPro-2.local:3000'); // for testing locally
 })();
